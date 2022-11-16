48576ea16d093e1cfe721d4db9e103185832f276
B214618 <email not provided>
2022-11-16 15:55:02 +0000

    A very roundabout way of making a request to the NCBI server and then extracting the query key and webenv key. Probably a better way of doing this...

--- scripts/myscript.py
+++ scripts/myscript.py
@@ -5,28 +5,37 @@ import configparser
 import argparse
 import sys
 import subprocess
-from urllib import request
+import requests
+import re
+
 
 # Initializing a parser object, to facilitate command line arguments to be passed to the program
-parser = argparse.ArgumentParser(
+args_parser = argparse.ArgumentParser(
     prog = "B214618's Protein conservation tool",
     description = "This program <content to be added>",
     epilog = "<content to be added>")
 
-parser.add_argument('--protein', dest='protein', default="pyruvate dehydrogenase")
-parser.add_argument('--group', dest='grouping', default="Ascomycota")
-parser.add_argument('--database', dest='database', default="protein")
+args_parser.add_argument('--protein', dest='protein', default="pyruvate dehydrogenase")
+args_parser.add_argument('--group', dest='grouping', default="Ascomycota")
+args_parser.add_argument('--database', dest='database', default="protein")
 
 # Takes an argument of an optional infile/outfile. if none is provided, it takes the standard input. 'nargs='?'' specifies that these are optional.
 #parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 #parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
 
-args = parser.parse_args()
-
+args = args_parser.parse_args()
 print(args.protein, args.grouping)
 
 #esearch_output = subprocess.check_output("esearch -db " + "\"" + args.database + "\"" +  " -query " + "\"" + args.protein + "\"", shell=True)
-with request.urlopen("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nucleotide&term=Cosmoscarta&usehistory=y") as f:
-     print(f.read().decode('utf-8'))
-
+with requests.get("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nucleotide&term=Cosmoscarta&usehistory=y") as f:
+    xml_file_byte = f.content
+    xml_file = xml_file_byte.decode('utf-8')
+    webenv_match = re.search('(?<=<WebEnv>)(.*)(?=<\/WebEnv>)', xml_file)
+    querykey_match = re.search('(?<=<QueryKey>)(.*)(?=<\/QueryKey>)', xml_file)
+    if webenv_match:
+        web_env = webenv_match.group(1)
+        print('WebEnv key:' + web_env)
+    if querykey_match:
+        query_key = querykey_match.group(1)
+        print('Query key:' + query_key)
 #print(esearch_output)
