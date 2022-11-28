4a5cb50eb89c7b7d206e017ddb004eedefa3a794
B214618 <email not provided>
2022-11-28 13:22:38 +0000

    Removing unnecessary files...

--- scripts/fetch.py
+++
@@ -1,6 +0,0 @@
-#!/bin/python3
-
-from subprocess import check_output
-
-test_output = check_output('esearch -db protein -query "pyruvate dehydrogenase" | efilter -organism "Durotheca rogersii" | efetch -format fasta', shell = True)
-print(test_output.decode('utf-8').strip('\n'))
